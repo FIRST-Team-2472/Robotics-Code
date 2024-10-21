9cd589732b415ee5d4ef8f1d8a24b9761d54238d
Warren Le <151572961+Warreneter@users.noreply.github.com>
2024-10-21 17:04:46 -0500

    Initial Commit

---
+++ RoboticsCode/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+
+
+package frc.robot;
+
+
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+  public static class MotorConstants {
+    public static final int kMotorID = 0;
+  }
+}
---
+++ RoboticsCode/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,29 @@
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import frc.robot.Constants.MotorConstants;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+  private final CANSparkMax Motor;
+
+  public ExampleSubsystem() {
+    Motor = new CANSparkMax(MotorConstants.kMotorID, CANSparkMax.MotorType.kBrushless); 
+  }
+
+  public void setMotorSpeed(double speed){
+    Motor.set(speed);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
