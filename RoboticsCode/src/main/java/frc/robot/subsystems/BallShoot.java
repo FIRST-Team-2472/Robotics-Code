292323309ac2379e85cdb8d6e7916aa67bb6a590
Warreneter <151572961+Warreneter@users.noreply.github.com>
2024-11-18 16:58:09 -0600

    Added subsystems.

---
+++ RoboticsCode/src/main/java/frc/robot/subsystems/BallShoot.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BallShoot extends SubsystemBase{
+    
+    public BallShoot(){
+
+    }
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+  
+    @Override
+    public void simulationPeriodic() {
+      // This method will be called once per scheduler run during simulation
+    }
+}
--- RoboticsCode/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ RoboticsCode/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import frc.robot.Constants.MotorConstants;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
